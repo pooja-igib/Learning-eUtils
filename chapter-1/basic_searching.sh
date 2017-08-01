1f6dba37137ea83633b1b496321c9b91404cddcc
pooja-igib <pooja.atri9@gmail.com>
2017-08-01 18:06:25 +0530

    added shell script

---
+++ chapter-1/basic_searching.sh
@@ -0,0 +1,3 @@
+#Get the PubMed IDs for articles about peripheral blood Published in science 2017
+
+wget https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=homo+sapiens+AND+peripheral+blood&maxdate=2017/07/31&retmode=JSON
